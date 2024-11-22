7bfcf8384417cdf53e034536c27fd11f2cb353fd
Jure Pustoslemšek <jure.poglednik@gmail.com>
2024-11-22 13:09:14 +0100

    outlines of a few theorems to prove

--- MasterTheorem/AsymptoticGrowth.lean
+++ MasterTheorem/AsymptoticGrowth.lean
@@ -14,3 +14,21 @@ def AsymptoticallyDominatedBy {L R : Type} [LinearOrder L] [LinearOrderedCommRin
 
 def AsymptoticallyBoundedBy {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) :=
   ∃ k₁ > 0, ∃ k₂ > 0, ∃ N, ∀ n > N, k₁ * |g n| ≤ |f n| ∧ |f n| ≤ k₂ * |g n|
+
+theorem asymp_dominated_implies_bounded_above {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (h : AsymptoticallyDominatedBy f g) : AsymptoticallyBoundedAboveBy f g := by
+  sorry
+
+theorem asymp_dominates_implies_bounded_below {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (h : AsymptoticallyDominates f g) : AsymptoticallyBoundedBelowBy f g := by
+  sorry
+
+theorem asymp_bounded_above_and_below_implies_bounded {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (ha : AsymptoticallyBoundedAboveBy f g) (hb : AsymptoticallyBoundedBelowBy f g) : AsymptoticallyBoundedBy f g := by
+  sorry
+
+theorem asymp_bounded_and_bounded_above_implies_not_dominated {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (h : AsymptoticallyBoundedBy f g) (ha : AsymptoticallyBoundedAboveBy f g) : ¬AsymptoticallyDominatedBy f g := by
+  sorry
+
+theorem asymp_bounded_and_bounded_below_implies_not_dominates {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (h : AsymptoticallyBoundedBy f g) (ha : AsymptoticallyBoundedBelowBy f g) : ¬AsymptoticallyDominates f g := by
+  sorry
+
+theorem not_asymp_dominates_and_dominated {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (h : AsymptoticallyDominates f g) (ha : AsymptoticallyDominatedBy f g) : False := by
+  sorry
--- MasterTheorem/BachmanLandauNotation.lean
+++ MasterTheorem/BachmanLandauNotation.lean
@@ -16,3 +16,21 @@ def o {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (g : L → R) :=
 
 def ω {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (g : L → R) := 
   { f : L → R | AsymptoticallyDominates f g }
+
+theorem o_implies_O {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (h : f ∈ o g) : f ∈ O g := by
+  sorry
+
+theorem ω_implies_Ω {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (h : f ∈ ω g) : f ∈ Ω g := by
+  sorry
+
+theorem O_and_Ω_implies_Θ {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (hO : f ∈ O g) (hΩ : f ∈ Ω g) : f ∈ Θ g := by
+  sorry
+
+theorem Θ_and_O_implies_not_o {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (hΘ : f ∈ Θ g) (hO : f ∈ O g) : ¬f ∈ o g := by
+  sorry
+
+theorem Θ_and_Ω_implies_not_ω {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (hΘ : f ∈ Θ g) (hΩ : f ∈ Ω g) : ¬f ∈ ω g := by
+  sorry
+
+theorem not_o_and_ω {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (ho : f ∈ o g) (hω : f ∈ ω g) : False := by
+  sorry
