3cdd71a1021a8cd286a6041d903f27405260fc69
Jure Pustoslemšek <jure.poglednik@gmail.com>
2024-11-28 00:08:18 +0100

    extract variables

--- MasterTheorem/AsymptoticGrowth.lean
+++ MasterTheorem/AsymptoticGrowth.lean
@@ -1,34 +1,36 @@
 import Mathlib.Algebra.Order.Ring.Abs
 
-def AsymptoticallyBoundedBelowBy {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) := 
+variable {L R : Type} [LinearOrder L] [LinearOrderedCommRing R]
+
+def AsymptoticallyBoundedBelowBy (f g : L → R) := 
   ∃ k > 0, ∃ N, ∀ n > N, |f n| ≥ k * |g n|
 
-def AsymptoticallyBoundedAboveBy {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) := 
+def AsymptoticallyBoundedAboveBy (f g : L → R) := 
   ∃ k > 0, ∃ N, ∀ n > N, |f n| ≤ k * |g n|
 
-def AsymptoticallyDominates {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) := 
+def AsymptoticallyDominates (f g : L → R) := 
   ∀ k > 0, ∃ N, ∀ n > N, |f n| ≥ k * |g n|
 
-def AsymptoticallyDominatedBy {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) := 
+def AsymptoticallyDominatedBy (f g : L → R) := 
   ∀ k > 0, ∃ N, ∀ n > N, |f n| ≤ k * |g n|
 
-def AsymptoticallyBoundedBy {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) :=
+def AsymptoticallyBoundedBy (f g : L → R) :=
   ∃ k₁ > 0, ∃ k₂ > 0, ∃ N, ∀ n > N, k₁ * |g n| ≤ |f n| ∧ |f n| ≤ k₂ * |g n|
 
-theorem asymp_dominated_implies_bounded_above {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (h : AsymptoticallyDominatedBy f g) : AsymptoticallyBoundedAboveBy f g := by
+theorem asymp_dominated_implies_bounded_above (f g : L → R) (h : AsymptoticallyDominatedBy f g) : AsymptoticallyBoundedAboveBy f g := by
   sorry
 
-theorem asymp_dominates_implies_bounded_below {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (h : AsymptoticallyDominates f g) : AsymptoticallyBoundedBelowBy f g := by
+theorem asymp_dominates_implies_bounded_below (f g : L → R) (h : AsymptoticallyDominates f g) : AsymptoticallyBoundedBelowBy f g := by
   sorry
 
-theorem asymp_bounded_above_and_below_implies_bounded {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (ha : AsymptoticallyBoundedAboveBy f g) (hb : AsymptoticallyBoundedBelowBy f g) : AsymptoticallyBoundedBy f g := by
+theorem asymp_bounded_above_and_below_implies_bounded (f g : L → R) (ha : AsymptoticallyBoundedAboveBy f g) (hb : AsymptoticallyBoundedBelowBy f g) : AsymptoticallyBoundedBy f g := by
   sorry
 
-theorem asymp_bounded_and_bounded_above_implies_not_dominated {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (h : AsymptoticallyBoundedBy f g) (ha : AsymptoticallyBoundedAboveBy f g) : ¬AsymptoticallyDominatedBy f g := by
+theorem asymp_bounded_and_bounded_above_implies_not_dominated (f g : L → R) (h : AsymptoticallyBoundedBy f g) (ha : AsymptoticallyBoundedAboveBy f g) : ¬AsymptoticallyDominatedBy f g := by
   sorry
 
-theorem asymp_bounded_and_bounded_below_implies_not_dominates {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (h : AsymptoticallyBoundedBy f g) (ha : AsymptoticallyBoundedBelowBy f g) : ¬AsymptoticallyDominates f g := by
+theorem asymp_bounded_and_bounded_below_implies_not_dominates (f g : L → R) (h : AsymptoticallyBoundedBy f g) (ha : AsymptoticallyBoundedBelowBy f g) : ¬AsymptoticallyDominates f g := by
   sorry
 
-theorem not_asymp_dominates_and_dominated {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (h : AsymptoticallyDominates f g) (ha : AsymptoticallyDominatedBy f g) : False := by
-  sorry
+theorem not_asymp_dominates_and_dominated (f g : L → R) (h : AsymptoticallyDominates f g) (ha : AsymptoticallyDominatedBy f g) : False := by
+  sorry 
--- MasterTheorem/BachmanLandauNotation.lean
+++ MasterTheorem/BachmanLandauNotation.lean
@@ -2,35 +2,149 @@ import Mathlib.Algebra.Order.Ring.Abs
 
 import MasterTheorem.AsymptoticGrowth
 
-def O {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (g : L → R) := 
+variable {L R : Type} [LinearOrder L] [LinearOrderedCommRing R]
+
+def O (g : L → R) := 
   { f : L → R | AsymptoticallyBoundedAboveBy f g }
 
-def Ω {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (g : L → R) := 
+def Ω (g : L → R) := 
   { f : L → R | AsymptoticallyBoundedBelowBy f g }
 
-def Θ {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (g : L → R) := 
+def Θ (g : L → R) := 
   { f : L → R | AsymptoticallyBoundedBy f g }
 
-def o {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (g : L → R) := 
+def o (g : L → R) := 
   { f : L → R | AsymptoticallyDominatedBy f g }
 
-def ω {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (g : L → R) := 
+def ω (g : L → R) := 
   { f : L → R | AsymptoticallyDominates f g }
 
-theorem o_implies_O {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (h : f ∈ o g) : f ∈ O g := by
+theorem o_implies_O (f g : L → R) (h : f ∈ o g) : f ∈ O g := by
   sorry
 
-theorem ω_implies_Ω {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (h : f ∈ ω g) : f ∈ Ω g := by
+theorem ω_implies_Ω (f g : L → R) (h : f ∈ ω g) : f ∈ Ω g := by
   sorry
 
-theorem O_and_Ω_implies_Θ {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (hO : f ∈ O g) (hΩ : f ∈ Ω g) : f ∈ Θ g := by
+theorem O_and_Ω_implies_Θ (f g : L → R) (hO : f ∈ O g) (hΩ : f ∈ Ω g) : f ∈ Θ g := by
   sorry
 
-theorem Θ_and_O_implies_not_o {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (hΘ : f ∈ Θ g) (hO : f ∈ O g) : ¬f ∈ o g := by
+theorem Θ_and_O_implies_not_o (f g : L → R) (hΘ : f ∈ Θ g) (hO : f ∈ O g) : ¬f ∈ o g := by
   sorry
 
-theorem Θ_and_Ω_implies_not_ω {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (hΘ : f ∈ Θ g) (hΩ : f ∈ Ω g) : ¬f ∈ ω g := by
+theorem Θ_and_Ω_implies_not_ω (f g : L → R) (hΘ : f ∈ Θ g) (hΩ : f ∈ Ω g) : ¬f ∈ ω g := by
   sorry
 
-theorem not_o_and_ω {L R : Type} [LinearOrder L] [LinearOrderedCommRing R] (f g : L → R) (ho : f ∈ o g) (hω : f ∈ ω g) : False := by
+theorem not_o_and_ω (f g : L → R) (ho : f ∈ o g) (hω : f ∈ ω g) : False := by
   sorry
+
+section LinearOrder
+section o
+
+instance (f : L → R) : Preorder (o f) :=
+  sorry
+
+instance (f : L → R) : PartialOrder (o f) :=
+  sorry
+
+instance (f : L → R) : Min (o f) :=
+  sorry
+
+instance (f : L → R) : Max (o f) :=
+  sorry
+
+instance (f : L → R) : Ord (o f) :=
+  sorry
+
+instance (f : L → R) : LinearOrder (o f) :=
+  sorry
+
+end o
+
+section O
+
+instance (f : L → R) : Preorder (O f) :=
+  sorry
+
+instance (f : L → R) : PartialOrder (O f) :=
+  sorry
+
+instance (f : L → R) : Min (O f) :=
+  sorry
+
+instance (f : L → R) : Max (O f) :=
+  sorry
+
+instance (f : L → R) : Ord (O f) :=
+  sorry
+
+instance (f : L → R) : LinearOrder (O f) :=
+  sorry
+
+end O
+
+section Ω
+
+instance (f : L → R) : Preorder (Ω f) :=
+  sorry
+
+instance (f : L → R) : PartialOrder (Ω f) :=
+  sorry
+
+instance (f : L → R) : Min (Ω f) :=
+  sorry
+
+instance (f : L → R) : Max (Ω f) :=
+  sorry
+
+instance (f : L → R) : Ord (Ω f) :=
+  sorry
+
+instance (f : L → R) : LinearOrder (Ω f) :=
+  sorry
+
+end Ω
+
+section ω
+
+instance (f : L → R) : Preorder (ω f) :=
+  sorry
+
+instance (f : L → R) : PartialOrder (ω f) :=
+  sorry
+
+instance (f : L → R) : Min (ω f) :=
+  sorry
+
+instance (f : L → R) : Max (ω f) :=
+  sorry
+
+instance (f : L → R) : Ord (ω f) :=
+  sorry
+
+instance (f : L → R) : LinearOrder (ω f) :=
+  sorry
+
+end ω
+
+section Θ
+
+instance (f : L → R) : Preorder (Θ f) :=
+  sorry
+
+instance (f : L → R) : PartialOrder (Θ f) :=
+  sorry
+
+instance (f : L → R) : Min (Θ f) :=
+  sorry
+
+instance (f : L → R) : Max (Θ f) :=
+  sorry
+
+instance (f : L → R) : Ord (Θ f) :=
+  sorry
+
+instance (f : L → R) : LinearOrder (Θ f) :=
+  sorry
+
+end Θ
+end LinearOrder
