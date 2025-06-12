9d53764ec8c7acfb8a5c8527c9c3a1e721bbb376
Jure Pustoslemšek <jure.poglednik@gmail.com>
2025-06-12 17:42:26 +0200

    actually fix missing file error

--- MasterTheorem.lean
+++ MasterTheorem.lean
@@ -1,6 +1,5 @@
 -- This module serves as the root of the `MasterTheorem` library.
 -- Import modules here that should be built as part of the library.
-import MasterTheorem.Basic
 import MasterTheorem.Aux
 import MasterTheorem.AsymptoticIneq
 import MasterTheorem.AsymptoticGrowth
